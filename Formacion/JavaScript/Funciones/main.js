f86d85e9d39081feeae3c0f8a990f81dd9179d4d
Yeison Cordoba <yk110716@gmail.com>
2022-12-29 02:04:40 -0500

    .

---
+++ Formacion/JavaScript/Funciones/main.js
@@ -0,0 +1,11 @@
+
+
+var resultado= "Capuchino";
+function cafetera(ingrediente1, ingrediente2) {
+    if(ingrediente1 == "Leche" && ingrediente2 == "cafe"){
+        return resultado;
+    }
+}
+
+var tasa = cafetera("leche");
+console.log(tasa)
